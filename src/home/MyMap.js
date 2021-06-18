c5313022a5340bb57960e34b154387b311f6c0e1
Khanh Nguyen Vu <k2.vu@student.vu.nl>
2021-06-18 17:53:24 +0200

    update: installation

--- src/home/MyMap.js
+++ src/home/MyMap.js
@@ -1,174 +1,78 @@
 import React, {Component} from "react";
-import 'ol/ol.css';
-import Feature from 'ol/Feature';
-import Map from 'ol/Map';
-import Point from 'ol/geom/Point';
-import VectorSource from 'ol/source/Vector';
-import View from 'ol/View';
-import {Style} from 'ol/style';
-import {Vector as VectorLayer} from 'ol/layer';
-import $ from 'jquery';
-import {Fill, RegularShape, Stroke} from 'ol/style';
-import {fromLonLat} from "ol/proj";
-import OSM from 'ol/source/OSM';
-import TileLayer from "ol/layer/Tile";
-import Geolocation from 'ol/Geolocation';
 
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import OSM from "ol/source/OSM";
+import Draw from "ol/interaction/Draw";
+import Modify from "ol/interaction/Modify";
 
-var stroke = new Stroke({color: 'black', width: 2});
-var fill = new Fill({color: 'red'});
-var styles = {
-    'square': new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            points: 4,
-            radius: 10,
-            angle: Math.PI / 4,
-        }),
-    }),
-    'rectangle': new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            radius: 10 / Math.SQRT2,
-            radius2: 10,
-            points: 4,
-            angle: 0,
-            scale: [1, 0.5],
-        }),
-    }),
-    'triangle': new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            points: 3,
-            radius: 10,
-            rotation: Math.PI / 4,
-            angle: 0,
-        }),
-    }),
-    'star': new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            points: 5,
-            radius: 10,
-            radius2: 4,
-            angle: 0,
-        }),
-    }),
-    'cross': new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            points: 4,
-            radius: 10,
-            radius2: 0,
-            angle: 0,
-        }),
-    }),
-    'x': new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            points: 4,
-            radius: 10,
-            radius2: 0,
-            angle: Math.PI / 4,
-        }),
-    }),
-    'stacked': [
-        new Style({
-            image: new RegularShape({
-                fill: fill,
-                stroke: stroke,
-                points: 4,
-                radius: 5,
-                angle: Math.PI / 4,
-                displacement: [0, 10],
-            }),
-        }),
-        new Style({
-            image: new RegularShape({
-                fill: fill,
-                stroke: stroke,
-                points: 4,
-                radius: 10,
-                angle: Math.PI / 4,
-            }),
-        })],
-};
-var styleKeys = [
-    'x',
-    'cross',
-    'star',
-    'triangle',
-    'square',
-    'rectangle',
-    'stacked'];
-var count = 50;
-var features = new Array(count);
-var e = 50000;
-const xOffset = -8900000;
-const yOffset = -30000;
-
-for (var i = 0; i < count; ++i) {
-    var coordinates = [2 * e * Math.random() - e + xOffset, 2 * e * Math.random() - e + yOffset];
-    features[i] = new Feature(new Point(coordinates));
-    features[i].setStyle(
-        styles[styleKeys[Math.floor(Math.random() * styleKeys.length)]]
-    );
-}
-
-var source = new VectorSource({
-    features: features,
+var raster = new TileLayer({
+    source: new OSM(),
 });
 
-var vectorLayer = new VectorLayer({
+var source = new VectorSource({wrapX: false});
+
+var vector = new VectorLayer({
     source: source,
 });
 
+const xOffset = -9040000;
+const yOffset = -505000;
+const mapSize = 15000;
+var draw, typeSelect;
 
 class PublicMap extends Component {
     constructor(props) {
         super(props);
-        this.state = {center: [0, 0], zoom: 1};
         this.map = new Map({
-            layers: [new TileLayer({
-                source: new OSM(),
-
-            }), vectorLayer],
-            target: document.getElementById('map'),
+            layers: [raster, vector],
             view: new View({
-                zoom: 5,
-                minZoom: 5,
+                extent: [xOffset-2*mapSize, yOffset -mapSize, xOffset+2*mapSize, yOffset +mapSize],
+                minZoom: 1,
+                zoom: 2,
                 maxZoom: 20,
+                center: [0, 0]
             }),
         });
-
-
-    }
-
-    updateMap() {
-        this.map.getView().setCenter(this.state.center);
-        this.map.getView().setZoom(this.state.zoom);
     }
 
     componentDidMount() {
-        this.map.setTarget("map");
-        // Listen to map changes
-        this.setState({center: [-8900000, -35000], zoom: 9});
+        this.map.setTarget("map")
+        var modify = new Modify({source: source});
+        this.map.addInteraction(modify);
+        this.addInteractions()
     }
 
-    userAction() {
-        this.setState({center: [146000, 1], zoom: 8});
+    addInteractions() {
+        typeSelect = document.getElementById("type")
+        draw = new Draw({
+            source: source,
+            type: typeSelect.value,
+        });
+        this.map.addInteraction(draw)
+    }
+
+    changeIcon() {
+        this.map.removeInteraction(draw)
+        this.addInteractions()
     }
 
     render() {
-        this.updateMap(); // Update map on render?
         return (
-            <div id="map" className="map" style={{width: "90%", height: "560px"}}></div>
+            <div>
+                <div id="map" className="map" style={{width: "90%", height: "560px"}}/>
+                <form className="form-inline">
+                    <label htmlFor="type">Geometry type &nbsp;</label>
+                    <select id="type" onChange={() => this.changeIcon()}>
+                        <option value="Point">Point</option>
+                        <option value="Polygon">Polygon</option>
+                        <option value="Circle">Circle</option>
+                    </select>
+                </form>
+            </div>
         );
     }
 }
